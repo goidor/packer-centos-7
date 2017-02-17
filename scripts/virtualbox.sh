9db7aea23809a71c0c3af8bd3b448fb0090607f0
Rodrigo Bravo <rodri.brv@gmail.com>
2017-02-17 20:35:19 +0100

    Moved scripts

--- scripts/virtualbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-rm -rf VBoxGuestAdditions.iso
